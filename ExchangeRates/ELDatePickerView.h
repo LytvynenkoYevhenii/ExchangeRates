c1bc94687c6ed277a304bcfe1d3c46df5cec5ab6
LytvynenkoYevhenii <litvinenko.evg@gmail.com>
2017-01-13 15:46:38 +0200

    Created DatePickerView

---
+++ ExchangeRates/ELDatePickerView.h
@@ -0,0 +1,14 @@
+//
+//  ELDatePickerView.h
+//  CurrenciesApp
+//
+//  Created by Lytvynenko Yevhenii on 11.01.17.
+//  Copyright © 2017 Lytvynenko Yevhenii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELDatePickerView : UIView
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+
+@end
