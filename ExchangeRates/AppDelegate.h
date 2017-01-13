5acf2fed685ee87bc4d12ca2adbef4c1e8634411
LytvynenkoYevhenii <litvinenko.evg@gmail.com>
2017-01-13 15:22:11 +0200

    Project is created

---
+++ ExchangeRates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExchangeRates
+//
+//  Created by Lytvynenko Yevhenii on 13.01.17.
+//  Copyright © 2017 Lytvynenko Yevhenii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExchangeRates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExchangeRates
+//
+//  Created by Lytvynenko Yevhenii on 13.01.17.
+//  Copyright © 2017 Lytvynenko Yevhenii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
